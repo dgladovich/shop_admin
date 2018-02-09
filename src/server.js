fec58ae99fc4226ccefc26b82dac9ac70a08bd77
dgladovich <dgladovich@gmail.com>
2018-02-09 17:32:38 +0200

    at last something starts works

--- src/server.js
+++ src/server.js
@@ -1,30 +1,22 @@
 import path from 'path';
 import express from 'express';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import expressJwt, { UnauthorizedError as Jwt401Error } from 'express-jwt';
 import expressGraphQL from 'express-graphql';
-import jwt from 'jsonwebtoken';
-import nodeFetch from 'node-fetch';
 import React from 'react';
-import ReactDOM from 'react-dom/server';
 import PrettyError from 'pretty-error';
-//import passport from './passport';
-import router from './router';
 import models from './data/models';
 import db from './db/models';
 import schema from './data/schema';
-//import assets from './assets.json'; // eslint-disable-line import/no-unresolved
 import config from './config';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import expressJwt, { UnauthorizedError as Jwt401Error } from 'express-jwt';
+//import jwt from 'jsonwebtoken';
+//import nodeFetch from 'node-fetch';
+//import assets from './assets.json'; // eslint-disable-line import/no-unresolved
+//import passport from './passport';
 
 const app = express();
-
-//
-// Tell any CSS tooling (such as Material UI) to use all vendor prefixes if the
-// user agent is not known.
-// -----------------------------------------------------------------------------
-global.navigator = global.navigator || {};
-global.navigator.userAgent = global.navigator.userAgent || 'all';
+console.log('server starting')
 
 //
 // Register Node.js middleware
@@ -37,15 +29,15 @@ app.use(bodyParser.json());
 //
 // Authentication
 // -----------------------------------------------------------------------------
-app.use(
+/*app.use(
   expressJwt({
     secret: config.auth.jwt.secret,
     credentialsRequired: false,
     getToken: req => req.cookies.id_token,
   }),
-);
+);*/
 // Error handler for express-jwt
-app.use((err, req, res, next) => {
+/*app.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   if (err instanceof Jwt401Error) {
     console.error('[express-jwt-error]', req.cooksies.id_token);
@@ -53,7 +45,7 @@ app.use((err, req, res, next) => {
     res.clearCookie('id_token');
   }
   next(err);
-});
+});*/
 
 //app.use(passport.initialize());
 
@@ -78,7 +70,8 @@ app.use(
 // -----------------------------------------------------------------------------
 app.get('*', async (req, res, next) => {
   try {
-
+    console.log('actioned some route');
+    res.send('some shit i send for test');
   } catch (err) {
     next(err);
   }
@@ -112,9 +105,8 @@ if (!module.hot) {
         });
     });
 }
-app.listen(3001)
 //
-// Hot Module Replacement
+// Hot Module Re placement
 // -----------------------------------------------------------------------------
 if (module.hot) {
   app.hot = module.hot;
