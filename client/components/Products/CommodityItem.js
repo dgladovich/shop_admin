441eefeb5648f1d50f3231f8dbb8540e96b667c7
dgladovich <dgladovich@gmail.com>
2018-05-11 14:14:51 +0300

    Editing product component

--- client/components/Products/CommodityItem.js
+++ client/components/Products/CommodityItem.js
@@ -3,18 +3,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './style/CommodityItem.scss';
+import CommodityUpdateDialog from './CommodityUpdateDialog';
 import {Grid, Cell, Card, CardTitle, CardText, CardActions, Button, IconButton} from 'react-mdl';
 
 
 export default class CommodityItem extends React.Component {
+    state = {
+        isOpenDialog: false
+    };
+    openEditWindow = ()=>{
+        this.setState({ isOpenDialog: true });
+    };
+    onCloseEditor = ()=>{
+      this.setState({ isOpenDialog: false });
+    };
+    deleteCommodity = ()=>{
 
+    };
     render() {
         return (
             <Cell cel={3} shadow={2} className={styles.commodity}>
-                <IconButton className={styles.editButton} name="build" />
-                <IconButton className={styles.deleteButton} name="delete_outline" />
+                <div className={styles.controllButtons}>
+                    <IconButton onClick={this.deleteCommodity.bind(this)} className={styles.deleteButton} name="delete_outline" />
+                    <IconButton onClick={this.openEditWindow.bind(this)} className={styles.editButton} name="build" />
+                </div>
                 <span>{this.props.name}</span>
                 <p>Price<span>{this.props.price}</span></p>
+                <CommodityUpdateDialog isOpen={this.state.isOpenDialog} onCloseEditor={this.onCloseEditor.bind(this)}/>
             </Cell>
         );
     }
